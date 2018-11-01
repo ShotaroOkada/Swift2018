e08671bf87df230c198e339b23c0ea7eb54ccc50
ShotaroOkada <tarou@enpit2017mba-06.local>
2018-11-01 15:21:41 +0900

    Initial Commit

---
+++ DoubleSidebar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoubleSidebar
+//
+//  Created by ShotaroOkada on 2018/11/01.
+//  Copyright © 2018年 ShotaroOkada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
